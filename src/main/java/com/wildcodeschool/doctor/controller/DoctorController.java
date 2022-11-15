bdaf245146d7beab4e12961baff5dedfdc6d2614
loicWCS <loic.lebon.wcs@gmail.com>
2022-11-15 12:34:19 +0100

    quest finish

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,17 +1,40 @@
 package com.wildcodeschool.doctor.controller;
 
 import com.wildcodeschool.doctor.model.Doctor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
-    @ResponseBody
-    public Doctor doctor() {
 
-        return null;
+    @GetMapping("/doctor/{number}")
+
+    @ResponseBody
+    public Doctor doctor(@PathVariable int number) {
+        if (number == 13) {
+            return new Doctor(13, "Jodie Whittaker");
+        } else if (number <= 12) {
+            throw new ResponseStatusException(HttpStatus.SEE_OTHER, "Doctor Not Found");
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    String.format("Impossible de récupérer l'incarnation %d", number));
+        }
+
     }
+
 }
+
+
+
+// @GetMapping("/questions/{id}")
+// Question getQuestion(@PathVariable int id) {
+// if (id == 0) {
+// throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Question Not Found");
+// }
+// return Questions.getById(id);
+// }
--- src/main/java/com/wildcodeschool/doctor/model/Doctor.java
+++ src/main/java/com/wildcodeschool/doctor/model/Doctor.java
@@ -5,8 +5,7 @@ public class Doctor {
     private int number;
     private String name;
 
-    public Doctor() {
-    }
+    public Doctor() {}
 
     public Doctor(int number, String name) {
         this.number = number;
